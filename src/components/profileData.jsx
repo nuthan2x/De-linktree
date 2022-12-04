fd04eed1f63e2ea86a725c7423d2f21bedea2f66
Nuthan Prasad <nuthan1159@gmail.com>
2022-12-04 11:26:11 +0530

    socials updata

--- src/components/profileData.jsx
+++ src/components/profileData.jsx
@@ -7,6 +7,7 @@ import {
   useNetwork,
 } from 'wagmi';
 import { FaCopy, FaStopCircle, FaExternalLinkAlt} from 'react-icons/fa';
+import axios from 'axios';
 
 const ProfileData = (props) => {
   const { address, isConnecting, isDisconnected } = useAccount()
@@ -16,14 +17,74 @@ const ProfileData = (props) => {
   const [newMessage, setnewMessage] = useState(undefined);
   const [txnStatus, setTxnStatus] = useState(undefined);
   const [amount, setamount] = useState(undefined);
+  const [newIpfsHash, setnewIpfsHash] = useState(undefined);
+  const [getIpfsHash, setgetIpfsHash] = useState(undefined);
+  const [NoData, setNoData] = useState(true);
+
+  const [socialData, setsocialData] = useState({
+    lensHandle : undefined,
+    twitterHandle : undefined,
+    blogHandle : undefined,
+    githubHandle : undefined,
+  });
 
   const{ensAvatar, ensName} = props
+
+  useEffect(() => {
+    getDATA(props.inputAddress)
+   
+  }, [address]);
 
   useEffect( () => {
    
     console.log(props.ensAvatar);
     setinputAddress(props.inputAddress)
+    getDATA(props.inputAddress)
+    setNoData(true)
   }, [props.inputAddress]);
+
+  const getDATA = async (address) => {
+    const options = {
+      method: 'GET',
+      url: 'https://deep-index.moralis.io/api/v2/0x457F118DB546040a0bB8e4798d17622193b2Ff07/logs',
+      params: {
+        chain: 'mumbai',
+        from_block: '29478720',
+        topic1: `0x000000000000000000000000${address?.slice(2,42)}`
+      },
+      headers: {accept: 'application/json', 'X-API-Key': 'OALLEXDPSYlwQ7u2A67gUCAY0TRLM5yjAVdjwHApeS1bnAlD03keIq9KpJDi8sG7'}
+    };
+    
+    
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log('response.data.total: ', response.data.total);
+        setgetIpfsHash(`https://gateway.pinata.cloud/ipfs/${hex_to_ascii(response.data?.result[0]?.data?.slice(130,258))}`);
+        
+        console.log('getIpfsHash: ', getIpfsHash);
+        fetch(`https://gateway.pinata.cloud/ipfs/${hex_to_ascii(response.data?.result[0]?.data?.slice(130,258))}`)
+        .then((response) => response.json())
+        .then((data) => setsocialData(data));
+
+        response.data.total > 0 && setNoData(false)
+      
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+  
+  function hex_to_ascii(str1)
+  {
+    var hex  = str1.toString();
+    var str = '';
+    for (var n = 0; n < hex.length; n += 2) {
+      str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+    }
+    return str;
+  }
 
   const getHexMessage = (str) => {
     var arr1 = [];
@@ -63,6 +124,35 @@ const ProfileData = (props) => {
         <h4>ENS avatar : </h4>
             <img src={props.ensAvatar} alt="" />
         </div>}
+
+        {!NoData && <div className="socialMedia">
+
+          <div>
+            <img src="https://files.readme.io/a0959e6-lens-logo1.svg" alt="" /> 
+            <h3>{socialData.lensHandle}</h3><div onClick={() =>  navigator.clipboard.writeText(socialData.lensHandle)}><FaCopy size={13}/></div>
+            <a href={socialData.lensHandle} target="_blank" rel="noopener noreferrer"><FaExternalLinkAlt size="10.5"/></a>
+          </div>
+
+          <div>
+            <img src="https://img.icons8.com/color/2x/twitter.png" alt="" /> 
+            <h3>{socialData.twitterHandle}</h3><div onClick={() =>  navigator.clipboard.writeText(socialData.twitterHandle)}><FaCopy size={13}/></div>
+            
+            <a href={socialData.twitterHandle} target="_blank" rel="noopener noreferrer"><FaExternalLinkAlt size="10.5"/></a>
+          </div>
+
+          <div>
+            <img src="https://img.icons8.com/ios-filled/2x/medium-logo.png" alt="" /> 
+            <h3>{socialData.blogHandle}</h3><div onClick={() =>  navigator.clipboard.writeText(socialData.blogHandle)}><FaCopy size={13}/></div>
+            <a href={socialData.blogHandle} target="_blank" rel="noopener noreferrer"><FaExternalLinkAlt size="10.5"/></a>
+          </div>        
+
+          <div>
+            <img src="https://img.icons8.com/sf-regular-filled/2x/github.png" alt="" /> 
+            <h3>{socialData.githubHandle}</h3><div onClick={() =>  navigator.clipboard.writeText(socialData.githubHandle)}><FaCopy size={13}/></div>
+            
+            <a href={socialData.githubHandle} target="_blank" rel="noopener noreferrer"><FaExternalLinkAlt size="10.5"/></a>
+          </div>
+          </div>}
 
       </div>
 
